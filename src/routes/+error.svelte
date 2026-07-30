2eac562664910860208ed26b8494e6bd28af98d0
0xF1 <0xf1dev@proton.me>
2026-07-30 12:20:49 +0200

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { page } from "$app/state";
+</script>
+
+<div id="wrapper">
+    <h1>error {page.status}</h1>
+    <span>{page.error?.message}</span>
+</div>
+
+<style>
+    #wrapper {
+        width: 100%;
+        height: 90%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
